d37ddbfd8b67a6a0daa8409dd707d5e6486caa9e
Novikov Andrey <andrnovi@green-book.local>
2014-06-20 18:26:39 +0300

    Fix test, and add work with unknow attribute

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,4 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/mpi_client.rb')
 
 include MPIClient
 
-MPIClient.server_url = 'https://secure.eticketsclub.com/3dsecure/xml'
+MPIClient.server_url = 'https://3dsecure.begateway.com/3dsecure/xml'
